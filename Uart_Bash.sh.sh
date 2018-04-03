aa9b4740717ecd739e8f6ac9d32c461100dcde8c
w0Pedro0w <37929417+w0Pedro0w@users.noreply.github.com>
2018-04-03 15:07:08 +0100

    Delete Uart_Bash.sh.sh

--- Uart_Bash.sh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "dtparam=uart0=on" >> /boot/config.txt
-echo "enable_uart = 1" >> /boot/config.txt
-sed -i "s/console=tty1//g" /boot/cmdline.txt
-sed -i "s/console=serial0/console=tty1/g" /boot/cmdline.txt
